28176bf3c7fdffcfde200010c1418f011da295fe
gonzalojpv <gonzalojpv@gmail.com>
2020-03-16 12:35:32 -0600

    GP, Dash, Card Component and styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">Catálogo</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <a href="#"  class="btn btn-link btn-lg btn-block">Users</a>
+                    <a href="#" class="btn btn-link btn-lg btn-block">Category</a>
+                    <a href="#" class="btn btn-link btn-lg btn-block">Products</a>
                 </div>
             </div>
         </div>

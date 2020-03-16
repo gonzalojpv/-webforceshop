09ef97b1a228d5ef63f3ec28a20939b4a35e8206
gonzalojpv <gonzalojpv@gmail.com>
2020-03-16 11:04:50 -0600

    GP, Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p>WebForceShop Todos los derechos reservados. 2020</p>
+            </div>
+        </div>
+    </div>
+</footer>

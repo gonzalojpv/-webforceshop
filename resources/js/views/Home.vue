7f0b35f3cbacf34165748d43a5de33e7dd7136a3
gonzalojpv <gonzalojpv@gmail.com>
2020-03-17 15:06:50 -0600

    GP, TextBlock Componente

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -2,17 +2,20 @@
     <section class="home">
         <Header/>
         <Slider/>
+        <TextBlock/>
     </section>
 </template>
 
 <script>
 import Header from '../components/Header';
 import Slider from '../components/Slider';
+import TextBlock from '../components/TextBlock';
 
 export default {
     components: {
         Header,
-        Slider
+        Slider,
+        TextBlock
     },
     mounted() {
         console.log('Holas');
